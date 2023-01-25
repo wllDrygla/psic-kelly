7e90e6ef7946c380cf105246a92c3dbd245aabaf
WiLL <wdryglati@gmail.com>
2023-01-25 18:20:23 -0300

    Inicio de tudo

---
+++ Posts.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var postSchema = new Schema({
+    titulo:String,
+    imagem:String,
+    descricao:String
+
+
+},{collection:'timeline'})
+
+var Posts = mongoose.model('Posts',postSchema);
+
+module.exports = Posts;
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+const path = require('path');
+
+const app = express();
+
+const Posts = require('./Posts.js');
+
+mongoose.connect("mongodb+srv://Will:mafg8859@cluster0.pjngibn.mongodb.net/will?retryWrites=true&w=majority",{useNewUrlParser:  true, useUnifiedTopology: true}).then(function(){
+    console.log('conectado com sucesso');
+}).catch(function(err){
+    console.log(err.message);
+})
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public',  express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res)=>{
+    console.log(req.query);
+
+    if(req.query.busca == null){
+        Posts.find({}).exec(function(err,timeline){
+            res.render('home', {timeline:timeline});
+
+        })
+
+    }else{
+        res.send('Você buscou por: '+req.query.busca);
+    }
+})
+
+app.get('/:slug', (req,res)=>{
+    res.send(req.params.slug)
+})
+
+app.listen(5000, ()=>{
+    console.log('rodando');
+})
